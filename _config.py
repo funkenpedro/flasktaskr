4456ce32164eaf055a750ea980f9a2bf0744d09c
Pedro Peres <pedro@ncf.ca>
2017-03-26 21:34:10 -0400

    app setup to show login page but not tasks.html

---
+++ _config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'something'
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
