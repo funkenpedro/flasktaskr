280b81595fb623fab76f36e253c7e9a00e846e5c
Pedro Peres <pedro@ncf.ca>
2017-03-27 22:05:34 -0400

    first round of Part2 tutorial with SQLalchemy created database.

--- _config.py
+++ _config.py
@@ -3,7 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = 'something'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+DATABASE_PATH
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+DATABASE_PATH
